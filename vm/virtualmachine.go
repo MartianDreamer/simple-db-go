3b03360315b29791a18f8922ae20e2399df83ff4
Sang Nguyen <nguysa02@risk.regn.net>
2022-08-31 11:24:20 +0700

    upgrade insert statement

--- vm/virtualmachine.go
+++ vm/virtualmachine.go
@@ -16,12 +16,13 @@ func ExecuteStatement(statement frontend.Statement) {
 		whereToWrite := TABLE.RowSlot(TABLE.RowNumber)
 		copy(whereToWrite[:], rowBytes[:])
 		TABLE.RowNumber += 1
+		fmt.Println("db > Executed.")
+		fmt.Printf("db > (%v, %v, %v)\n", insertRow.Id, string(insertRow.Username[:]), string(insertRow.Email[:]))
 	case frontend.SelectStatement:
-		fmt.Println(statement.Content)
 		lastRowBytes := TABLE.RowSlot(TABLE.RowNumber - 1)
 		var lastRow [295]byte
 		copy(lastRow[:], lastRowBytes[:295])
 		convertedRow := frontend.BytesToRow(lastRow)
-		fmt.Printf("%v %v %v \n", convertedRow.Id, convertedRow.Username, convertedRow.Email)
+		fmt.Printf("%v %v %v \n", convertedRow.Id, string(convertedRow.Username[:]), string(convertedRow.Email[:]))
 	}
 }
